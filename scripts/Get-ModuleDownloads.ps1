4de0d63b91e68dd96238b362ee0f3761c44614a2
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2023-11-23 10:15:37 +0100

    Update Get-ModuleDownloads.ps1
    * Created a function, so the script won't use variables from the previous run. Should fix #1 .

--- scripts/Get-ModuleDownloads.ps1
+++ scripts/Get-ModuleDownloads.ps1
@@ -1,15 +1,27 @@
-$PSGalleryProfile = Invoke-WebRequest -Uri "https://www.powershellgallery.com/profiles/$env:PSGALLERY_PROFILE_NAME"
-$ModuleList = $PSGalleryProfile.links.href.where({
-    $_ -like "/packages/*/"
-}) -replace "(\/[A-Za-z]+\/)|(\/)",""
+function Get-ModuleDownloads {
+    param (
+        [Parameter(Mandatory)][string]$ModuleName
+    )
 
-$UnixTime = Get-Date -UFormat %s
+    $ModuleURI = "https://www.powershellgallery.com/packages/$ModuleName"
 
-$ModuleList.ForEach({
-    $ModuleURI = "https://www.powershellgallery.com/packages/$_"
-    $ModulePage = ((Invoke-WebRequest -Uri $ModuleURI | ConvertFrom-Html).InnerText -split '\r?\n').Trim()
+    try {
+        $ModulePage = ((Invoke-WebRequest -Uri $ModuleURI | ConvertFrom-Html).InnerText -split '\r?\n').Trim()
+    }
+    catch {
+        Write-Error -ErrorAction Stop -Message @"
+Got error while trying to fetch `"$ModuleName`".
+------------------------------------------------
+$_
+"@
+    }
+
     $TotalDownloadsIndex = $ModulePage.IndexOf("Downloads")
     [int]$ModuleDownloads = $ModulePage[$TotalDownloadsIndex-3]
+
+    if (!$ModuleDownloads) {
+        return
+    }
 
     $Splat = @{
         "Uri" = "$env:INFLUX_HOST/api/v2/write?bucket=$env:INFLUX_BUCKET&org=$env:INFLUX_ORG&precision=s"
@@ -21,4 +33,15 @@ $ModuleList.ForEach({
         "Body" = "count,source=psgallery,module_name=$_ downloads=$ModuleDownloads $UnixTime"
     }
     Invoke-RestMethod @Splat
+}
+
+$PSGalleryProfile = Invoke-WebRequest -Uri "https://www.powershellgallery.com/profiles/$env:PSGALLERY_PROFILE_NAME"
+$ModuleList = $PSGalleryProfile.links.href.where({
+    $_ -like "/packages/*/"
+}) -replace "(\/[A-Za-z]+\/)|(\/)",""
+
+$UnixTime = Get-Date -UFormat %s
+
+$ModuleList.ForEach({
+    Get-ModuleDownloads -ModuleName $_
 })
